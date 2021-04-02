fce930bd1b64569552beeae67a1264f55d05a42c
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-02 05:16:52 +0400

    added react dependences, react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <Switch>
+            <Route exact path="/">
+                <h1>
+                    here is home page
+                </h1>
+            </Route>
+            <Route exact path="/starred">
+                <h1>
+                    here is page for starred movies
+                </h1>
+            </Route>
+            <Route>
+                <h1>
+                    here is error:404 page
+                </h1>
+            </Route>
+        </Switch>
+    )
 }
 
 export default App
