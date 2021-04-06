f8fbadd1f3479e059c794471b29f6bf6b09ac155
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-07 03:05:39 +0400

    created input and fetch results from tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
-const home = () => {
+const Home = () => {
+    const [input, setInput] = useState('');
+    
+    const onSearch = () => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+        .then(r => r.json())
+        .then(result => console.log(result))
+    }
+
+    const onInputChange = (ev) => {
+        setInput(ev.target.value);
+    }
+    
+    const onKeyDown = (ev) => {
+        if(ev.keyCode === 13){
+            onSearch()
+        }
+    }
+
     return (
         <MainPageLayout>
-            here is home page
+            <input type="search" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type='button' onClick={onSearch} >Search</button>
         </MainPageLayout>
     )
 }
 
-export default home
+export default Home;
