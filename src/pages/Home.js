546ce669e5f154eaebcf3cb1dcc112b99e54d46b
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-12 00:27:39 +0400

    created preview cards for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import {apiGet} from '../misc/config'
 
 const Home = () => {
@@ -34,9 +36,7 @@ const Home = () => {
             return <div>no result</div>
         };
         if(result && result.length > 0) {
-            return result[0].show 
-            ? result.map( (item)=> <div key={item.show.id}> {item.show.name} </div> )
-            : result.map( (item)=> <div key={item.person.id}> {item.person.name} </div> )
+            return result[0].show ? <ShowGrid data={result} /> : <ActorGrid data={result} />
         };
         return null;
     };
