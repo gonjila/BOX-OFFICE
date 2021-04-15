e52e4712c1a82e2dabee6a93a44a6ef889cc2b60
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-15 05:03:20 +0400

    created show page components and populate them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,9 @@
 import React, {useEffect, useReducer} from 'react'
 import {useParams} from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 
@@ -22,39 +26,29 @@ const initialState = {
     error: null
 }
 
+
 const Show = () => {
     const { id } = useParams();
 
     const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState)
     
-    // const [show, setShow] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [error, setError] = useState(null)
-
     useEffect( ()=>{
         let isMounted = true;
-
+        
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-
             if(isMounted){
-
                 dispatch( { type:'FETCH_SUCCESS', show: results } )
-
             }
-
         }).catch(err => {
             if(isMounted){
-
                 dispatch( { type:'FETCH_FAILED', error: err.message } )
-            
             }
         })
-
+        
         return () => {
             isMounted = false;
         }
     }, [id] )
-    console.log(`show`, show)
 
     if(isLoading){
         return <div>Data is being loaded</div>
@@ -65,7 +59,20 @@ const Show = () => {
 
     return (
         <div>
-            This is show page  
+            <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres} /> 
+
+            <div>
+                <h2>Details</h2>
+                <Details status={show.status} network={show.network} premiered={show.premiered} />
+            </div>
+            <div>
+                <h2>Seasons</h2>
+                <Seasons seasons={show._embedded.seasons} />
+            </div>
+            <div>
+                <h2>Cast</h2>
+                <Cast cast={show._embedded.cast} />
+            </div>
         </div>
     )
 }
