381bfcc14c93346aa64cca55d2de25366d6eb924
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-15 05:36:38 +0400

    styled show page

--- src/components/show/Details.js
+++ src/components/show/Details.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { DetailsWrapper } from './Details.styled';
 
 const Details = ({ status, premiered, network }) => {
     return (
-      <div>
-        <p>
-          Status: <span>{status}</span>
-        </p>
-        <p>
-          Premiered {premiered} {network ? `on ${network.name}` : null}
-        </p>
-      </div>
+        <DetailsWrapper>
+            <p>
+                Status: <span>{status}</span>
+            </p>
+            <p>
+                Premiered {premiered} {network ? `on ${network.name}` : null}
+            </p>
+        </DetailsWrapper>
     );
-  };
+};
 
 export default Details
