9788d2ee8b23b6b48c3da63d2a972f702698a3ed
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-21 02:27:06 +0400

    define theme for styled components and created a few styled components

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { LinkStyled, NavList } from './Navs.styled';
+
+
+const LINKS = [
+    {to: '/', text: 'Home'},
+    {to: '/starred', text: 'starred'}
+]
+
 
 const navs = () => {
-
-    const LINKS = [
-        {to: '/', text: 'Home'},
-        {to: '/starred', text: 'starred'}
-    ]
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const location = useLocation();
 
     return (
         <div>
-            <ul>
+            <NavList>
                 {
-                    LINKS.map(page => <li key={page.to}><Link to={page.to}>{page.text}</Link></li>)
+                    LINKS.map(page => <li key={page.to}><LinkStyled className={page.to === location.pathname ? 'active' : ""} to={page.to}>{page.text}</LinkStyled></li>)
                 }
-            </ul>
+            </NavList>
         </div>
     )
 }
