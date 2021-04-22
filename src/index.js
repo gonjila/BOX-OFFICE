21a37f8d38646563d88fa695cfcb96da8e421441
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-22 23:30:07 +0400

    site deployment and changed BrowserRouter into HashRouter in the index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>,
+ReactDOM.render(<HashRouter> <App /> </HashRouter>,
     document.getElementById('root')
 );
 
