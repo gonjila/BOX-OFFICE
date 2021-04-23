056f382c7831446f03ec06ccfb4ffe8a2d6986cc
gonjila <gonjilashvili.lasha@yahoo.com>
2021-04-24 02:06:08 +0400

    added serviceWorker.js and changed api's link into HTTPS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<HashRouter> <App /> </HashRouter>,
     document.getElementById('root')
 );
 
-reportWebVitals();
+serviceWorker.register();
